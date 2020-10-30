3037748e9c3b6dc463c885ae09edc9411d6b75c3
Luis Huayta <developer1@mediabyte.com.pe>
2020-10-30 17:20:19 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {HTTP_METHODS, headers, serverRoutes} from "./app.constants";
+import {Grid} from "@material-ui/core";
 
 function App() {
+
+  useEffect(()=>{
+
+    const applicationHeaders = new Headers()
+
+    applicationHeaders.append(headers.x_role_id,process.env.REACT_APP_X_ROLE_ADMIN_ID)
+    applicationHeaders.append(headers.contentType,process.env.REACT_APP_CONTENT_TYPE)
+    applicationHeaders.append(headers.x_type_operation_id,"726f6c65")
+
+
+    fetch(buildUrl(process.env.REACT_APP_URL_BASE,serverRoutes.settings,'list?') + new URLSearchParams({
+      size : 10,
+      page : 1
+    }),{
+      method: HTTP_METHODS.GET,
+      headers:applicationHeaders
+    }).then((response)=>{
+      if(!response.ok) throw  response
+      console.log(response)
+      return response.json()
+    }).then((data)=>{
+      setStatus(data)
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+
+      </div>
   );
 }
 
---
+++ src/app.constants.js
@@ -0,0 +1,20 @@
+export const HTTP_METHODS = {
+    GET:'GET',
+    POST:'POST',
+    PUT:'PUT',
+    DELETE:'DELETE'
+}
+
+export const headers = {
+    x_type_operation_id : 'x-type-operation_id',
+    x_role_id : 'x-role-id',
+    contentType : 'Content-Type'
+}
+
+export const serverRoutes = {
+    authentication:'api/authentication',
+    courses :'api/courses',
+    teachers:'api/teachers',
+    settings :'api/settings',
+    admins: 'api/admin'
+}
